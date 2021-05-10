7656cc52c63c245e6461d451b24f69a331dca60e
SamanthaNguyen1 <>
2021-05-10 03:47:52 +0000

    Comments added

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-// everything is set to 0
+// Everything is set to 0 so it can change based on user input
 let bA = 0
 let bB = 0
 let height = 0
 let area = 0
 
-// when the button is clicked, the math calculate
+// This makes the button react once it is clicked
 document.getElementById('button').addEventListener('click', math)
 
-// this is how to solve the area of a trapezoid
+// These are the commands to calculate the area and when the calculate button is clicked, it will send an alert of the area calculated
 function math () {
-  bA = document.getElementById('baseA').value
+  bA = document.getElementById('base-a').value
   bA = parseInt(bA)
-  bB = document.getElementById('baseB').value
+  bB = document.getElementById('base-b').value
   bB = parseInt(bB)
   height = document.getElementById('height').value
   height = parseInt(height)
